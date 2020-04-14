e57c0afa525ba9220953924533eebc1fb43ae2ba
Zheleznikov <polwen@yandex.ru>
2020-04-14 12:42:44 +0300

    step2 изменения перед клонированием репозитория

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
+const { celebrate, Joi, errors } = require('celebrate');
+
 const config = require('./config');
 
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
@@ -25,6 +28,7 @@ mongoose.connect(config.CONNECTION_ADDRESS, {
   useUnifiedTopology: true,
 });
 
+app.use(requestLogger);
 app.post('/signin', login);
 app.post('/signup', createUser);
 
@@ -32,6 +36,21 @@ app.use('/', auth, require('./routes/users'));
 app.use('/', auth, require('./routes/cards'));
 
 app.use((req, res) => res.status(404).send({ message: 'Запрашиваемый ресурс не найден' }));
+
+app.use(errorLogger);
+
+app.use(errors());
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка' : message,
+    });
+});
 
 app.listen(config.PORT, () => {
   console.log(`App listening on port ${config.PORT}`);
