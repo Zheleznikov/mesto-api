faaa22bf3dc2a3f124f35c44edb6a23c62f73eef
Zheleznikov <polwen@yandex.ru>
2020-04-16 09:51:16 +0300

    step 7 add celebrates every route

--- routes/cards.js
+++ routes/cards.js
@@ -2,10 +2,36 @@ const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { getCards, createCard, deleteCard, likeCard, dislikeCard } = require('../controllers/cards');
 
+// получить все карточки
 router.get('/cards', getCards);
-router.post('/cards', createCard);
-router.delete('/cards/:cardId', deleteCard);
-router.put('/cards/:cardId/likes', likeCard);
-router.delete('/cards/:cardId/likes', dislikeCard);
+
+// добавить карточку
+router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().uri(),
+  }),
+}), createCard);
+
+// удалить карточку
+router.delete('/cards/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), deleteCard);
+
+// поставить лайк
+router.put('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), likeCard);
+
+// убрать лайк
+router.delete('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), dislikeCard);
 
 module.exports = router;
